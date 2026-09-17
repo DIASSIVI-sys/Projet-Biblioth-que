f8f72a50526d92ecc755f0bc6254fc0be51e96fc
DIASSIVI-sys <nsangoudjenna1@gmail.com>
2026-09-17 22:56:50 +0100

    fix:suppresion des fichiers doubles

--- src/routes/stats.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const statsController = require('../controllers/stats.controller');
-
-//GET Api/stats
-router.get('/',statsController.getStats);
-
-module.exports = router;
