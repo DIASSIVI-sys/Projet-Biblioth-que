4882e619f59dd200b10e0db282eebf5c7c37bce2
DIASSIVI-sys <nsangoudjenna1@gmail.com>
2026-09-17 20:29:22 +0100

    fix: suppression des doublons a la racine

--- src/middlewares/logger.js
+++
@@ -1,7 +0,0 @@
-const logger=(req, res, next)=>{
-    const date =new Date ().toISOString();
-    console.log(`${date} - ${req.method} ${req.originalUrl}`);
-    next();
-};
-
-module.exports=logger;
